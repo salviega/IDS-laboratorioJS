11e36299e61684330326e0ba18fa3aa3a6ded941
salviega <salviega6@gmail.com>
2022-04-21 07:28:49 -0500

    Tercer  avance del lab JavaScript, se realizo la movilid de las barras

--- pong/main.js
+++ pong/main.js
@@ -26,14 +26,15 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
         down: function() {
-
+            this.y += this.speed;
         },
         up: function() {
-
+            this.y -= this.speed;
         }
     }
 })();
@@ -67,13 +68,24 @@
     }
 })();
 
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar = new Bar(735,100,40,100,board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas,board);
+
+document.addEventListener("keydown", function(ev){
+    if(ev.keycode == 38) {
+        bar.up();
+    }
+    else if(ev.keycode == 40) {
+        bar.down();
+    }
+});
+
 window.addEventListener("load", main);
 function main() {
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(735,100,40,100,board);
-    var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas,board);
+    
     board_view.draw();
 
 }
