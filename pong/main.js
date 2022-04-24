29bd1e56a38ad5a5a91cfd5e45e0aa00c1f962c8
salviega <salviega6@gmail.com>
2022-04-24 15:22:15 -0500

    último commit, la pelota colisiona con las barras laterales

--- pong/main.js
+++ pong/main.js
@@ -24,15 +24,35 @@
         this.radius = radius;
         this.speed_y = 0;
         this.speed_x = 3;
-        board.ball = this;
         this.board = board;
-        this.kind = "circle"
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
+        board.ball = this;
+        this.kind = "circle"
+
     }
     self.Ball.prototype = {
         move: function() {
             this.x += (this.speed_x * this.direction);
-            this.y += (this.speed_y * this.direction);
+            this.y += (this.speed_y);
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision: function(bar) {
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+            if(this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 })();
@@ -54,6 +74,9 @@
         },
         up: function() {
             this.y -= this.speed;
+        },
+        toString: function() {
+            return "x: " + this.x + " y: " + this.y;
         }
     }
 })();
@@ -74,18 +97,52 @@
         draw: function() {
             for (var i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
-                draw(this.ctx,el)
+                
+                draw(this.ctx,el);
             };
+        },
+        check_collsions: function() {
+            for (var i = this.board.bars.length - 1; i>= 0; i--) {
+                var bar = this.board.bars[i];
+                if(hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
         },
         play: function() {
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collsions();
                 this.board.ball.move();
             }
         }
     }
-
+    function hit(a,b) {
+        var hit = false;
+        //Colisiones verticales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width) 
+        {
+            if(b.y + b.height >= a.y && b.y < a.y + a.height)
+                console.log(':3')
+                hit = true;
+        }
+        //Colision de a con b
+        if(b.x <= a.x && b.x + b.width >= a.x + a.width) 
+        {
+            if(b.y <= a.y && b.y + b.height >= a.y + a.height)
+                console.log(':D  ')
+                hit = true;
+        }
+        //Colision b con a
+        if(a.x <= b.x && a.x + a.width >= b.x + b.width) 
+        {
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height) 
+                console.log(':S')    
+                hit = true;
+        }
+        return hit;
+    }
     function draw(ctx,element) {
             switch(element.kind) {
                 case "rectangle":
@@ -110,6 +167,7 @@ var board_view = new BoardView(canvas,board);
 var ball = new Ball(350,100,10,board);
 
 document.addEventListener("keydown", function(ev){
+    
     if(ev.keyCode == 38) {
         ev.preventDefault();
         bar.up();
@@ -126,18 +184,17 @@ document.addEventListener("keydown", function(ev){
         ev.preventDefault();
         bar_2.down();
     }
-    else if(ev.keyCode ===32) {
+    else if(ev.keyCode === 32) {
         ev.preventDefault();
         board.playing = !board.playing;
     }
 });
 board_view.draw();
+
 window.requestAnimationFrame(controller);
-setTimeout(function(){
-    ball.direction = -1;
-},4000);
+
 function controller() {
     board_view.play();
-    window.requestAnimationFrame(controller);
+    requestAnimationFrame(controller);
 
 }
